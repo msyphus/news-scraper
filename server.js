b56f2ba40e623b1169e8475503c6681e7c705b95
msyphus <mark.syphus@gmail.com>
2020-06-27 23:07:15 -0600

    built wireframe and basic functionality

--- server.js
+++ server.js
@@ -14,8 +14,36 @@ app.use(express.static("public"));
 
 mongoose.connect(MONGODB_URI)
 
+app.get("/scrape", function(req, res) {
+    axios.get("https://www.undercurrentnews.com/upstream/aquaculture/").then(function(response) {
+        const $ = cheerio.load(response.data);
+        $("article a").each(function(i, element) {
+            let result = {};
+            result.title = $(element).find("a").attr("title");
+            result.link = $(element).find("a").attr("href");
 
+            db.Articles.create(result)
+                .then(function(dbArticle) {
+                    console.log(dbArticle);
+                })
+                .catch(function(err) {
+                    console.log(err);
+                });
+        });
+    });
+    res.send("Scrape complete!");
+});
 
+app.get("/articles", function(req, res) {
+    db.Articles.find({})
+    .populate("Articles")
+    .then(function(dbArticles) {
+        res.json(dbArticles);
+    })
+    .catch(function(err) {
+        res.json(err);
+    });
+});
 
 app.listen(PORT, () => {
     console.log("Connected to Port " + PORT);
